380a6668eb6ed75c3e57b5d1d2aca2ad5fad97e5
Aqib-dev05 <aqibgithub@gmail.com>
2025-08-15 11:47:46 +0500

    first api created

---
+++ ExpreeJS/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors"; // Changed this line
+import fs from "fs";
+const app = express();
+app.use(cors());
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/api/users", (req, res) => {
+    fs.readFile("users.json", "utf-8", (err, data) => {
+        if (err) {
+            console.error("Error reading file:", err);
+            res.status(500).json({ error: "Internal Server Error" });
+            return;
+        }
+        const users = JSON.parse(data);
+        res.json(users);
+    });
+});
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`);
+});
