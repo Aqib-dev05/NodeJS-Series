456f3c1e39c2ca1b7c77b70b8c83394ceab91538
Aqib-dev05 <aqibgithub@gmail.com>
2025-09-07 11:47:27 +0500

    z

--- 1st_Project/index.js
+++ 1st_Project/index.js
@@ -1,10 +1,18 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import mongoose from "mongoose";
 
- const PORT = 3000;
+const PORT = 3000;
 
-  const app =express();
+const app = express();
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
 
-  app.listen(PORT,()=>{
-    console.log("Listening at the port",PORT)
-  })
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log("Listening at the port", PORT);
+});
